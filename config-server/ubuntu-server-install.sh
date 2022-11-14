955ef8113d937876d2b30cd0ac74c1e123e9a397
hermel <mail.hermel@gmail.com>
2022-11-14 16:11:44 +0100

    making some lines green

--- config-server/ubuntu-server-install.sh
+++ config-server/ubuntu-server-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail # fail on error and report it, debug all lines
-GREEN='\033[0;31m'
-RED='\033[0;31m'
+export GREEN='\033[0;31m'
+export RED='\033[0;31m'
 sudo -n true
 test $? -eq 0 || exit 1 "${RED} you should have sudo privilege to run this script"
 
