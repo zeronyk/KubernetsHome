480671a264de3e4c7b52b13ccfb66a653c0afd88
hermel <mail.hermel@gmail.com>
2022-11-14 16:11:13 +0100

    making some lines green

--- config-server/ubuntu-server-install.sh
+++ config-server/ubuntu-server-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
 set -eu -o pipefail # fail on error and report it, debug all lines
-
+GREEN='\033[0;31m'
+RED='\033[0;31m'
 sudo -n true
-test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+test $? -eq 0 || exit 1 "${RED} you should have sudo privilege to run this script"
 
-echo '\033[0;32m making all updates and adding repos'
+echo '${GREEN} making all updates and adding repos'
 
 apt-get update -y
 apt-get dist-upgrade -y
@@ -16,7 +16,7 @@ CRIO_VERSION=1.23
 
 
 
-echo "\033[0;32m Assuming your desired CRIO_VERSION is $CRIO_VERSION and you operate on UBUNTU 22.04 or 20.04 "
+echo "${GREEN} Assuming your desired CRIO_VERSION is $CRIO_VERSION and you operate on UBUNTU 22.04 or 20.04 "
 
 
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"| tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
@@ -28,10 +28,10 @@ curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:sta
 apt update 
 apt install -y cri-o cri-o-runc
 
-echo "\033[0;32m Starting Crio services, Start Crio on startup"
+echo "${GREEN} Starting Crio services, Start Crio on startup"
 systemctl enable crio.service
 systemctl start crio.service
-echo " \033[0;32m Install more prequisits"
+echo "${GREEN} Install more prequisits"
 apt install -y cri-tools apt-transport-https ca-certificates
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -41,7 +41,7 @@ apt-mark hold kubelet kubeadm kubectl
 
 
 
-echo "\033[0;32m creating cgroup-file"
+echo "${GREEN} creating cgroup-file"
 echo "# kubeadm-config.yaml
 kind: ClusterConfiguration
 apiVersion: kubeadm.k8s.io/v1beta3
@@ -55,5 +55,5 @@ apt-get update && sudo apt-get upgrade
 modprobe br_netfilter
 
 
-echo "\033[0;32m Kubelet was installed and started check status with 'systemctl status kubelet'"
-echo "\033[0;32m Join an existing cluster with by running 'kubeadm token create --print-join-command' on master node"
+echo "${GREEN} Kubelet was installed and started check status with 'systemctl status kubelet'"
+echo "${GREEN} Join an existing cluster with by running 'kubeadm token create --print-join-command' on master node"
